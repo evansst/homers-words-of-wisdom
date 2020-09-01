7def477ba855aa22571b514e28722ce00600cb72
Sam Evans <evansst@icloud.com>
2020-09-01 15:57:22 -0600

    builds out login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 
 export default class Login extends Component {
   state = {
-
+    email: '',
+    password: '',
   }
 
 
   render() {
     return (
       <div className="Login">
-        <form onSubmit={this.props.handleLogin}>
-          <input type="text" ></input>
+        <form onSubmit={this.props.handleLogin} formData={this.state}>
+          <input type="text" name="email" value={this.state.email} />
+          <input type="text" name="password" value={this.state.password} />
         </form>
       </div>
     )
+  }
+
+  handleEmailChange = (event) => {
+
+  }
+  handleEmailChange = (event) => {
+
   }
 }
